601c082faaf95aa6020ecc0be280fc325649c3f7
ADMIN <ADMIN@ADMIN-PC>
2022-01-02 20:26:02 +0530

    space has added

--- src/main/java/PomClasses/UsersPage.java
+++ src/main/java/PomClasses/UsersPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UsersPage {
+
 	private WebDriver driver;
 	private WebDriverWait wait;
 	
